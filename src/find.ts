2f9ef84fba48fdc8c430ea692d607516eacef07f
wxn0brP <toja.filiper@gmail.com>
2026-05-17 21:58:11 +0200

    fix: $ operator

--- src/find.ts
+++ src/find.ts
@@ -9,15 +9,15 @@ export async function find(slv: SQLiteValthera, config: VQueryT.Find): Promise<D
 
     let sqlResult = [];
 
-    if (typeof search === "function" || Object.keys(search).length === 0) {
+    const baseKeys = Object.keys(search)
+        .filter(k => search[k] !== undefined)
+        .filter(k => !k.startsWith("$"))
+        .filter(k => typeof search[k] !== "object");
+
+    if (typeof search === "function" || baseKeys.length === 0) {
         const stmt = await slv._prepare(`SELECT * FROM ${collection}`);
         sqlResult = await Promise.resolve(stmt.all());
     } else {
-        const baseKeys = Object.keys(search)
-            .filter(k => search[k] !== undefined)
-            .filter(k => !k.startsWith("$"))
-            .filter(k => typeof search[k] !== "object");
-
         const baseSql = `SELECT * FROM ${collection} WHERE ${baseKeys.map(k => `${k} = ?`).join(" AND ")}`;
         const baseValues = baseKeys.map(k => toSqlValue(search[k]));
         const stmt = await slv._prepare(baseSql);
